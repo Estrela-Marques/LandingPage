a01e389a02eaa5df6801c1cfbb040f799df8a3f8
Estrela Marques <estelapfarma@gmail.com>
2022-11-07 21:03:08 -0300

    Adicionando página "erro" e imagem.

---
+++ erro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Erro</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-icons.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="card rounded-0 border-secondary my-5 p-0 shadow">
+        <div class="row">
+          <div class="row align-middle border-secondary col-md-6"><img src="img/chuteira-bg-form_dmot5u.avif" class="img-fluid rounded-0 d-none d-none d-md-block" alt="Mãos segurando um par de chuteira vermelha e preta"></div>
+          <div class="col-md-6 center">
+            <div class="card-body mt-4 p-4 text-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+              </svg>
+              <h5 class="text-uppercase mt-4 mb-4 lh-1" margin-10><em><strong>NÃO FOI POSSÍVEL REALIZAR O CADASTRO, TENTE NOVAMENTE MAIS TARDE.</strong></em></h5>
+              <form action="index.php" method="get">
+                <div class="col-12">
+                  <button class="btn btn-dark rounded-0 border-dark col-12 col-sm-6" type="submit">Voltar para a página inicial!</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
