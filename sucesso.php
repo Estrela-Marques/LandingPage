b47a1ed96b2b9166e6134f8ea16d4adcca005067
Estrela Marques <estelapfarma@gmail.com>
2022-11-08 08:04:56 -0300

    Adicionando página "sucesso"

---
+++ sucesso.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sucesso</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-icons.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="card rounded-0 border-secondary my-5 p-0 shadow">
+        <div class="row">
+          <div class="row align-middle border-secondary col-md-6"><img src="img/chuteira-bg-form_dmot5u.avif" class="img-fluid rounded-0 d-none d-none d-md-block" alt="Mãos segurando um par de chuteira vermelha e preta"></div>
+          <div class="col-md-6 center">
+            <div class="card-body mt-4 p-4 text-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+              </svg>
+              <h5 class="text-uppercase mt-4 mb-4 lh-1" margin-10><em><strong>CADASTRO REALIZADO COM SUCESSO</strong></em></h5>
+              <form action="index.php" method="get">
+                <div class="col-12">
+                  <button class="btn btn-dark rounded-0 border-dark col-12 col-sm-6" type="submit">Voltar para a página inicial!</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
